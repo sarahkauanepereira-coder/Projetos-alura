2549977048c8942aa22d9365bfee000500796ec3
sarahkauanepereira-coder <sarah.kauane.pereira@escola.pr.gov.br>
2026-06-17 07:55:41 -0300

    .

---
+++ style.css
@@ -0,0 +1,15 @@
+cartao {
+    /* Valores antigos */
+    width: 300px;
+    height: auto;
+    padding: 10px;
+    background-color: #f0f0f0;
+}
+
+.cartao {
+    /* Novos valores para testar */
+    width: 260px;       /* Cartão um pouco mais estreito */
+    height: 200px;      /* Altura fixa (cuidado para o texto não vazar!) */
+    padding: 15px 25px; /* Mais espaço interno nas laterais */
+    background-color: #f0f0f0;
+}by
